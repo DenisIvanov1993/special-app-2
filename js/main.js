58c9e46d141bce364664216675aea8ca114f5311
DenisIvanov1993 <162470639+DenisIvanov1993@users.noreply.github.com>
2024-03-14 20:57:58 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(function(){
+
+    let showPopup = function(){
+        $('.popup-container').css('display', 'flex');
+    };
+
+    let closePopup = function(){
+        $('.popup-container').css('display', 'none');
+    };
+    $('.order-call').on('click', function(){
+        showPopup();
+    });
+
+    $('.close').on('click', function(){
+        clearForm();
+        closePopup();
+    });
+});
